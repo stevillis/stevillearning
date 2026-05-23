e5c9f1eb87b99647c4f365090998dda45bf91dd1
Stévillis Sousa <stevillis@hotmail.com>
2026-05-22 23:53:00 -0400

    chore: upgrade django to 5.2.14, update dependencies, and clean up settings

--- stevillis_site/settings.py
+++ stevillis_site/settings.py
@@ -137,8 +137,6 @@ TIME_ZONE = "America/Cuiaba"
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
@@ -151,8 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # MEDIA_URL = "/media/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
